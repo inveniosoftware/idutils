eb80d92c394c86249f2e52c0d66cfd8949972c2d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-06-26 23:08:37 +0200

    fix docs compatibilty problem with Sphinx>=5.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@
 #
 # This file is part of IDUtils
 # Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # IDUtils is free software; you can redistribute it and/or modify
 # it under the terms of the Revised BSD License; see LICENSE file for
@@ -77,7 +78,7 @@ release = __version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
