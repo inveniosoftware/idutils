97ea0dc943039adf22542110bab7e663e6163363
Alex Ioannidis <a.ioannidis@cern.ch>
2022-10-05 10:56:27 +0200

    isbn: fix cases where masking fails
    * Fallbacks to a canonical ISBN value in case the hyphenation/masking
      fails.

--- idutils/__init__.py
+++ idutils/__init__.py
@@ -835,7 +835,9 @@ def normalize_isbn(val):
     """
     if is_isbn10(val):
         val = isbnlib.to_isbn13(val)
-    return isbnlib.mask(isbnlib.canonical(val))
+    canonical = isbnlib.canonical(val)
+    masked = isbnlib.mask(canonical)
+    return masked or canonical
 
 
 def normalize_issn(val):
