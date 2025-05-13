4ba323ba549ef8c69dd73ce5d8ea86ab0fa69263
Will Riley <wanderingwill@gmail.com>
2025-05-13 15:39:51 +0200

    Simplified MIT license mention as suggested.

--- idutils/utils.py
+++ idutils/utils.py
@@ -358,34 +358,10 @@ See https://www.ebi.ac.uk/arrayexpress/help/accession_codes.html
 ascl_regexp = re.compile(r"^ascl:[0-9]{4}\.[0-9]{3,4}$", flags=re.I)
 """ASCL regular expression."""
 
-"""
-MIT License
 
-Copyright (c) 2023 abnf-to-regexp AUTHORS
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-Please see the file AUTHORS for the full list of the authors of abnf-to-regexp.
-"""
-
-
-# adapted from https://github.com/aas-core-works/abnf-to-regexp/blob/main/test_data/nested-python/rfc3987/expected.py
+# The following code is adapted from abnf-to-regexp (https://github.com/aas-core-works/abnf-to-regexp)
+# Copyright (c) 2023 abnf-to-regexp AUTHORS (see https://github.com/aas-core-works/abnf-to-regexp/blob/main/AUTHORS)
+# License: MIT (https://opensource.org/licenses/MIT)
 def _create_rfc3987_reg_exps():
     scheme = "[a-zA-Z][a-zA-Z0-9+\\-.]*"
     ucschar = (
