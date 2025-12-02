010612b469719521dde001d211a439a913103761
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-12-02 21:55:54 +0100

    feat: improve import usage
    * the warning was not working as intended.
    
    * since it is complicated programmatically to distinguish between
      'import idutils' and 'from idutils import XY' to write a warning
      only on one of the both cases and it is not the packages business
      to decide how the package which is using it will import functions
      from modules, the whole warning has been removed and the user can
      decide how it will be imported
    
    * relying only on 'from idutils.module_a import func_xy' would mean that
      all all packages which are using this have to rewrite there code with
      not really any real benefit
    
    * all those points lead to the removal of the warning. btw this behavior
      is the same now as it was with the warning.

--- idutils/__init__.py
+++ idutils/__init__.py
@@ -17,34 +17,11 @@
 
 """Small library for persistent identifiers used in scholarly communication."""
 
-import pkgutil
-from importlib import import_module
-from warnings import warn
+from .detectors import *
+from .normalizers import *
+from .proxies import *
+from .schemes import *
+from .utils import *
+from .validators import *
 
 __version__ = "1.5.0"
-
-
-def import_module_by_name(name):
-    """Import module."""
-    package_name = __name__
-    for _, file_name, _ in pkgutil.walk_packages(__path__):
-        module = import_module(f".{file_name}", package_name)
-
-        for attribute_name in dir(module):
-            if attribute_name == name:
-                return getattr(module, attribute_name)
-
-
-def __getattr__(name: str):
-    """__getattr__"""
-    try:
-        return import_module(f".{name}", __name__)
-    except ImportError:
-        warn(
-            f"Accessing '{name}' via the package namespace idutils is deprecated. "
-            f"Please use 'from idutils.moduleA import {name}' instead. "
-            "Namespace access will be removed in a future version.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return import_module_by_name(name)
